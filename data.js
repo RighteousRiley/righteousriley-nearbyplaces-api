b257d2a56677389a8b3d995b45fce0328c0146b5
RighteousRiley <rcsimpso@gmail.com>
2020-12-09 20:53:31 -0700

    Testing json response

---
+++ data.js
@@ -0,0 +1,38 @@
+[
+    {
+        name: "Applebee's Grill + Bar",
+        hasDelivery: "Yes",
+        hasTakeOut: "Yes",
+        address: "5870 East Broadway",
+        phoneNum: "(520) 750-9780",
+        type: "American, Sports Bar",
+        thumb: applePic
+    },
+    {
+        name: "Joey's Taco Shop",
+        hasDelivery: "Yes",
+        hasTakeOut: "Yes",
+        address: "123 South Campbell",
+        phoneNum: "(520) 125-1538",
+        type: "Mexican, Small Plates",
+        thumb: taco
+    },
+    {
+        name: "Wildcat Kitchen",
+        hasDelivery: "Yes",
+        hasTakeOut: "Yes",
+        address: "1658 N Cherry Ave",
+        phoneNum: "(520) 986-8943",
+        type: "Variety, Grill",
+        thumb: wildcat
+    },
+    {
+        name: "Roger's Lair",
+        hasDelivery: "Yes",
+        hasTakeOut: "Yes",
+        address: "123 S Kent St",
+        phoneNum: "(520) 712-7649",
+        type: "BBQ, American",
+        thumb: lair
+    }
+]
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 var cors = require('cors')
-let data = {}
+var bodyParser = require('body-parser');
+let data = require('./data')
 const app = express()
 const port = process.env.PORT || 3002
 
+app.use('cors')
+app.use(bodyParser)
 
 app.get('/', (request, response) =>  {
     response.send("Gamer, you made it")
@@ -14,7 +17,7 @@ app.post('/place', (request, response) => {
 })
 
 app.get('/places', (request, response) => {
-
+    response.json(data)
 })
 
 app.get('/search/:searchTerm/:location', (request, response) => {
